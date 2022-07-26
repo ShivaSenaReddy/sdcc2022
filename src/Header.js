b81a7508e1c268ff7e789d3f5ae8f81295e2f7d0
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-07-26 14:25:37 +0530

    changes

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return ( <div>
     <h1>Hungry Cinema</h1>
-        <h2>San Dieogo ComicCon2022</h2>
+        <h2>San Dieogo ComicCon2022 Announcements</h2>
         </div>)
 }
