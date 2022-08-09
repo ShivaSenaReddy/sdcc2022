05d8670c296161083816bca5f93149b0b06ff851
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-08-09 17:09:11 +0530

    changing header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 export default function Header() {
     return (<div className='Header'>
-    <h1>Hungry Cinema</h1>
+        <h1>Hungry Cinema</h1>
+        <p>One Stop Destination for all movie lovers</p>
         <h2>San Dieogo ComicCon2022 Announcements</h2>
         </div>)
 }
